7180ccdb194048648f6dc3eb8287970b5a010da7
edufouleur <edufouleur@gmail.com>
2026-08-28 11:28:23 +0200

    Update TmdbApi.kt

--- app/src/main/java/fr/dvdtheque/app/network/TmdbApi.kt
+++ app/src/main/java/fr/dvdtheque/app/network/TmdbApi.kt
@@ -6,21 +6,37 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TmdbApi {
+
     @GET("search/movie")
     suspend fun searchMovies(
-        @Header("Authorization") authorization: String,
-        @Query("query") query: String,
-        @Query("language") language: String = "fr-FR",
-        @Query("include_adult") includeAdult: Boolean = false,
-        @Query("page") page: Int = 1
+        @Header("Authorization")
+        authorization: String,
+
+        @Query("query")
+        query: String,
+
+        @Query("language")
+        language: String = "fr-FR",
+
+        @Query("include_adult")
+        includeAdult: Boolean = false,
+
+        @Query("page")
+        page: Int = 1
     ): TmdbSearchResponse
 
     @GET("movie/{movieId}")
     suspend fun movieDetails(
-        @Header("Authorization") authorization: String,
-        @Path("movieId") movieId: Int,
-        @Query("language") language: String = "fr-FR",
-        @Query("append_to_response") appendToResponse: String = "credits"
+        @Header("Authorization")
+        authorization: String,
+
+        @Path("movieId")
+        movieId: Int,
+
+        @Query("language")
+        language: String = "fr-FR",
+
+        @Query("append_to_response")
+        appendToResponse: String = "credits"
     ): TmdbMovieDetails
 }
-
