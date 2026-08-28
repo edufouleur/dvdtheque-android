907054ecb7a27968a6bd174cc720a6619bdba294
edufouleur <edufouleur@gmail.com>
2026-08-28 11:19:41 +0200

    Update TmdbApi.kt

--- app/src/main/java/fr/dvdtheque/app/network/TmdbApi.kt
+++ app/src/main/java/fr/dvdtheque/app/network/TmdbApi.kt
@@ -1,1 +1,26 @@
+package fr.dvdtheque.app.network
+
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TmdbApi {
+    @GET("search/movie")
+    suspend fun searchMovies(
+        @Header("Authorization") authorization: String,
+        @Query("query") query: String,
+        @Query("language") language: String = "fr-FR",
+        @Query("include_adult") includeAdult: Boolean = false,
+        @Query("page") page: Int = 1
+    ): TmdbSearchResponse
+
+    @GET("movie/{movieId}")
+    suspend fun movieDetails(
+        @Header("Authorization") authorization: String,
+        @Path("movieId") movieId: Int,
+        @Query("language") language: String = "fr-FR",
+        @Query("append_to_response") appendToResponse: String = "credits"
+    ): TmdbMovieDetails
+}
 
